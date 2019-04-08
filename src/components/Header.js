39f5fa91f3d17cecbb962ecada449cbbfd758e7f
tahmidk <tahmidkhan3141@gmail.com>
2019-04-07 23:20:28 -0700

    Favicon update

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ function Header() {
 				href="https://fonts.googleapis.com/css?family=Raleway:300"
 				rel="stylesheet"
 			/>
+			<link rel="icon" href="../favicon.png" />
 			<meta
 				content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
 				name="viewport"
