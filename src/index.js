c41c0298db437d875cbae6622a70ca6759b87241
tahmidk <tahmidkhan3141@gmail.com>
2019-04-08 12:15:05 -0700

    Removed service worker

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import "./styles/styles_main.css";
-//import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-//serviceWorker.unregister();
